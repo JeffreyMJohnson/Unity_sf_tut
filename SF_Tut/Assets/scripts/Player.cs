18be1a96462b80f28ef0e4d69bd26e6d4cec3be9
JeffreyMJohnson <jefjohms@gmail.com>
2015-05-15 07:59:41 -0700

    Have jab and cross animations done. refactoring into single code base for both players.

---
+++ SF_Tut/Assets/scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+
+    Vector3 currentDirection = Vector3.left;
+    float moveSpeed = 5;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleUI();
+    }
+
+    void HandleUI()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Move(Vector3.left);
+            
+
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            Move(Vector3.right);
+        }
+    }
+
+    void Move(Vector3 direction)
+    {
+        if (currentDirection != direction)
+        {
+            Debug.Log("flip it.");
+            transform.Rotate(Vector3.up, 180.0f);
+            currentDirection = direction;
+        }
+        
+        Vector3 dx = currentDirection * moveSpeed * Time.deltaTime;
+        Debug.Log("direction: " + dx);
+        transform.Translate(dx, Space.World);
+    }
+}
