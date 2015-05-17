0f37dc80b8c5aa06605dc2689d9264c5833db734
JeffreyMJohnson <jefjohms@gmail.com>
2015-05-17 15:24:17 -0700

    refactored into single shared class, now wondering if I should have.

--- SF_Tut/Assets/scripts/HitboxManager.cs
+++ SF_Tut/Assets/scripts/HitboxManager.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class HitboxManager : MonoBehaviour {
+public class HitboxManager : MonoBehaviour
+{
 
     //set these in the editor
-    
+
     public BoxCollider2D jab0;
     public BoxCollider2D jab1;
 
@@ -22,10 +23,10 @@ public class HitboxManager : MonoBehaviour {
         CLEAR //special case to remove all boxes
     }
 
-	// Use this for initialization
-	void Start () 
+    // Use this for initialization
+    void Start()
     {
-	    //set up the array so script can more easiloy set up boxes
+        //set up the array so script can more easiloy set up boxes
         colliders = new BoxCollider2D[] { jab0, jab1 };
 
         //create polygon collider
@@ -33,16 +34,24 @@ public class HitboxManager : MonoBehaviour {
         localCollider.isTrigger = true; //set as a trigger so it doesn't collide with environment
         //localCollider.pathCount = 0;//clear auto-generated polygons
         localCollider.size = Vector2.zero;//clear auto-generated polygons
-	}
+    }
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        Debug.Log("Collider hit something.");
+        if (localCollider.size.x > 0.01 && localCollider.size.y > 0.01)
+        {
+            //hitting mode
+            //this object hitting so fire the damage animation on who collided with
+            col.gameObject.GetComponent<Animator>().SetTrigger("faceDamage");
+        }
+
+
+
     }
 
     public void SetHitBox(HITBOXES value)
     {
-        if(value != HITBOXES.CLEAR)
+        if (value != HITBOXES.CLEAR)
         {
             //Debug.Log("setting box: " + value);
             //localCollider.SetPath(0, colliders[(int)value].GetPath(0));
@@ -54,9 +63,10 @@ public class HitboxManager : MonoBehaviour {
         //Debug.Log("setting box: " + value);
         localCollider.size = Vector2.zero;
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
